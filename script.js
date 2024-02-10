f76768219bd72f5833c1956ffdf3d7b53ef8bc8f
EmannuelMartinezTest <rubyroyproductions@gmail.com>
2024-02-09 20:35:22 -0600

    Update design architecture

--- script.js
+++ script.js
@@ -1,63 +1,90 @@
 "use strict";
-let main = document.querySelector(".book-container");
-let dialog = document.querySelector("#addBookDialog");
-let addBookButton = document.querySelector(".new-book");
-let form = document.querySelector("form");
-let closeModal = document.querySelector("#close-modal");
-let myLibrary = [];
-addBookButton.addEventListener("click", () => {
-    dialog.showModal();
-});
-closeModal.addEventListener("click", (_) => {
-    dialog.close();
-});
-form.addEventListener("submit", processBook);
-function removeBookFromList(evt) {
+let books = (function () {
+    let myLibrary = [];
+    // cache DOM
+    let main = document.querySelector(".book-container");
+    let dialog = document.querySelector("#addBookDialog");
+    let addBookButton = document.querySelector(".new-book");
+    let form = document.querySelector("form");
+    let closeModal = document.querySelector("#close-modal");
+    // bind events
+    addBookButton.addEventListener("click", dialogShowModal);
+    function makeBook(title, author, pages, read) {
+        return {
+            title,
+            author,
+            pages,
+            read,
+        };
+    }
+    /*
+        Initial library
+     */
+    const theHobbit = makeBook("The Hobbit", "J.R.R. Tolkien", 295, false);
+    const theStranger = makeBook("The Stranger", "Albert Camus", 125, true);
     // prettier-ignore
-    if (!(evt.target instanceof HTMLElement && evt.target.parentElement instanceof HTMLElement))
-        return;
-    let bookParentContainer = evt.target.parentElement;
-    let bookDeletionIndex = Number(bookParentContainer.getAttribute("data-index"));
-    updateLists(bookDeletionIndex, bookParentContainer);
-}
-// prettier-ignore
-function updateLists(bookDeletionIndex, bookParentContainer) {
-    myLibrary.splice(bookDeletionIndex, 1);
-    main.removeChild(bookParentContainer);
+    const callMeByYourName = makeBook("Call Me By Your Name", "Andre Aciman", 230, true);
+    addBookToLibrary(theHobbit);
+    addBookToLibrary(theStranger);
+    addBookToLibrary(callMeByYourName);
     displayLibrary();
-}
-function processBook(evt) {
-    evt.preventDefault();
-    const title = getInputElementValue("#book-title");
-    const author = getInputElementValue("#book-author");
-    const pages = Number(getInputElementValue("#book-pages"));
-    const read = getCheckedElementBool("#book-read");
-    const book = new Book(title, author, pages, read);
-    if (!checkRenderedBooks(book)) {
-        addBookToLibrary([book]);
+    // functions
+    function dialogCloseModal() {
+        closeModal.removeEventListener("click", dialogCloseModal);
+        form.removeEventListener("submit", processBook);
+        dialog.close();
+    }
+    function dialogShowModal() {
+        closeModal.addEventListener("click", dialogCloseModal);
+        form.addEventListener("submit", processBook);
+        dialog.showModal();
+    }
+    function removeBookFromList(evt) {
+        // prettier-ignore
+        if (!(evt.target instanceof HTMLElement && evt.target.parentElement instanceof HTMLElement))
+            return;
+        let bookParentContainer = evt.target.parentElement;
+        let bookDeletionIndex = Number(bookParentContainer.getAttribute("data-index"));
+        updateLists(bookDeletionIndex, bookParentContainer);
+    }
+    // prettier-ignore
+    function updateLists(bookDeletionIndex, bookParentContainer) {
+        myLibrary.splice(bookDeletionIndex, 1);
+        main.removeChild(bookParentContainer);
         displayLibrary();
     }
-    dialog.close();
-}
-function getInputElementValue(selector) {
-    const element = document.querySelector(selector);
-    return element ? element.value : "";
-}
-function getCheckedElementBool(selector) {
-    const element = document.querySelector(selector);
-    return element ? element.checked : false;
-}
-function addBookToLibrary(books) {
-    myLibrary = [...myLibrary, ...books];
-}
-// prettier-ignore
-function createSelectDropdownWidget(readStatus) {
-    const statuses = ["To Read", "Read", "Currently Reading"];
-    const selectedIndex = readStatus ? 1 : 0;
-    const optionsHTML = statuses
-        .map((status, index) => `<option value="${status}" ${index === selectedIndex ? "selected" : ""}>${status}</option>`)
-        .join("");
-    return `
+    function processBook(evt) {
+        evt.preventDefault();
+        const title = getInputElementValue("#book-title");
+        const author = getInputElementValue("#book-author");
+        const pages = Number(getInputElementValue("#book-pages"));
+        const read = getCheckedElementBool("#book-read");
+        const book = makeBook(title, author, pages, read);
+        if (!checkRenderedBooks(book)) {
+            addBookToLibrary(book);
+            displayLibrary();
+        }
+        dialog.close();
+    }
+    function getInputElementValue(selector) {
+        const element = document.querySelector(selector);
+        return element ? element.value : "";
+    }
+    function getCheckedElementBool(selector) {
+        const element = document.querySelector(selector);
+        return element ? element.checked : false;
+    }
+    function addBookToLibrary(book) {
+        myLibrary = [...myLibrary, book];
+    }
+    // prettier-ignore
+    function createSelectDropdownWidget(readStatus) {
+        const statuses = ["To Read", "Read", "Currently Reading"];
+        const selectedIndex = readStatus ? 1 : 0;
+        const optionsHTML = statuses
+            .map((status, index) => `<option value="${status}" ${index === selectedIndex ? "selected" : ""}>${status}</option>`)
+            .join("");
+        return `
     <div class="select">
         <select>
             ${optionsHTML}
@@ -65,37 +92,37 @@ function createSelectDropdownWidget(readStatus) {
         <span class="focus"></span>
     </div>
   `;
-}
-function updateDataIndexAttribute() {
-    let books = document.querySelectorAll(".book");
-    for (let [index, book] of books.entries()) {
-        book.setAttribute("data-index", String(index));
     }
-}
-// prettier-ignore
-function attachEventListenersToClass(className, eventType, eventHandler) {
-    const elements = document.querySelectorAll("." + className);
-    elements.forEach((element) => {
-        element.removeEventListener(eventType, eventHandler);
-        element.addEventListener(eventType, eventHandler);
-    });
-}
-function removeBookEventHandler(evt) {
-    removeBookFromList(evt);
-}
-function displayLibrary() {
-    myLibrary.forEach((book, index) => {
-        if (!checkRenderedBooks(book)) {
-            const bookCardHTML = createBookCard(book, index);
-            main.insertAdjacentHTML("beforeend", bookCardHTML);
+    function updateDataIndexAttribute() {
+        let books = document.querySelectorAll(".book");
+        for (let [index, book] of books.entries()) {
+            book.setAttribute("data-index", String(index));
         }
-    });
-    updateDataIndexAttribute();
-    attachEventListenersToClass("delete-book", "click", removeBookEventHandler);
-}
-function createBookCard(book, index) {
-    const readStatusDropdown = createSelectDropdownWidget(book.read);
-    return `
+    }
+    // prettier-ignore
+    function attachEventListenersToClass(className, eventType, eventHandler) {
+        const elements = document.querySelectorAll("." + className);
+        elements.forEach((element) => {
+            element.removeEventListener(eventType, eventHandler);
+            element.addEventListener(eventType, eventHandler);
+        });
+    }
+    function removeBookEventHandler(evt) {
+        removeBookFromList(evt);
+    }
+    function displayLibrary() {
+        myLibrary.forEach((book, index) => {
+            if (!checkRenderedBooks(book)) {
+                const bookCardHTML = createBookCard(book, index);
+                main.insertAdjacentHTML("beforeend", bookCardHTML);
+            }
+        });
+        updateDataIndexAttribute();
+        attachEventListenersToClass("delete-book", "click", removeBookEventHandler);
+    }
+    function createBookCard(book, index) {
+        const readStatusDropdown = createSelectDropdownWidget(book.read);
+        return `
     <div class="book" data-index="${index}">
         <div class="book-info">
             <div class="title">${book.title}</div>
@@ -105,32 +132,20 @@ function createBookCard(book, index) {
         </div>
         <button class="delete-book">x</button>
     `;
-}
-function checkRenderedBooks(book) {
-    for (let bookContainer of main.children) {
-        let [bookTitle, bookAuthor] = [
-            bookContainer.children[0].children[0].textContent,
-            bookContainer.children[0].children[1].textContent,
-        ];
-        if (bookTitle === book.title && bookAuthor === book.author)
-            return true;
     }
-    return false;
-}
-class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
+    function checkRenderedBooks(book) {
+        var _a, _b;
+        for (let bookContainer of main.children) {
+            let bookTitle = (_a = bookContainer.querySelector(".title")) === null || _a === void 0 ? void 0 : _a.textContent;
+            let bookAuthor = (_b = bookContainer.querySelector(".author")) === null || _b === void 0 ? void 0 : _b.textContent;
+            if (bookTitle === book.title && bookAuthor === book.author)
+                return true;
+        }
+        return false;
     }
-}
-/*
-    Initial library
- */
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-const theStranger = new Book("The Stranger", "Albert Camus", 125, true);
-// prettier-ignore
-const callMeByYourName = new Book("Call Me By Your Name", "Andre Aciman", 230, true);
-addBookToLibrary([theHobbit, theStranger, callMeByYourName]);
-displayLibrary();
+    return {
+        makeBook,
+        addBookToLibrary,
+        displayLibrary,
+    };
+})();
--- script.ts
+++ script.ts
@@ -1,77 +1,121 @@
-let main = document.querySelector(".book-container") as HTMLElement;
-let dialog = document.querySelector("#addBookDialog") as HTMLDialogElement;
-let addBookButton = document.querySelector(".new-book") as HTMLButtonElement;
-let form = document.querySelector("form") as HTMLFormElement;
-let closeModal = document.querySelector("#close-modal") as HTMLButtonElement;
+let books = (function () {
+  let myLibrary: Book[] = [];
 
-let myLibrary: Book[] = [];
+  // cache DOM
+  let main = document.querySelector(".book-container") as HTMLElement;
+  let dialog = document.querySelector("#addBookDialog") as HTMLDialogElement;
+  let addBookButton = document.querySelector(".new-book") as HTMLButtonElement;
+  let form = document.querySelector("form") as HTMLFormElement;
+  let closeModal = document.querySelector("#close-modal") as HTMLButtonElement;
 
-addBookButton.addEventListener("click", () => {
-  dialog.showModal();
-});
+  // bind events
+  addBookButton.addEventListener("click", dialogShowModal);
 
-closeModal.addEventListener("click", (_) => {
-  dialog.close();
-});
+  interface Book {
+    readonly title: string;
+    readonly author: string;
+    readonly pages: number;
+    readonly read: boolean;
+  }
 
-form.addEventListener("submit", processBook);
+  function makeBook(
+    title: string,
+    author: string,
+    pages: number,
+    read: boolean,
+  ): Book {
+    return {
+      title,
+      author,
+      pages,
+      read,
+    };
+  }
 
-function removeBookFromList(evt: Event) {
+  /*
+      Initial library
+   */
+
+  const theHobbit = makeBook("The Hobbit", "J.R.R. Tolkien", 295, false);
+
+  const theStranger = makeBook("The Stranger", "Albert Camus", 125, true);
   // prettier-ignore
-  if(!(evt.target instanceof HTMLElement && evt.target.parentElement instanceof HTMLElement) ) return;
-  let bookParentContainer = evt.target.parentElement;
-  let bookDeletionIndex = Number(
-    bookParentContainer.getAttribute("data-index"),
-  );
-  updateLists(bookDeletionIndex, bookParentContainer);
-}
+  const callMeByYourName = makeBook("Call Me By Your Name", "Andre Aciman", 230, true);
+  addBookToLibrary(theHobbit);
+  addBookToLibrary(theStranger);
+  addBookToLibrary(callMeByYourName);
 
-// prettier-ignore
-function updateLists(bookDeletionIndex: number, bookParentContainer: HTMLElement) {
-  myLibrary.splice(bookDeletionIndex, 1);
-  main.removeChild(bookParentContainer);
   displayLibrary();
-}
 
-function processBook(evt: Event) {
-  evt.preventDefault();
+  // functions
+  function dialogCloseModal() {
+    closeModal.removeEventListener("click", dialogCloseModal);
+    form.removeEventListener("submit", processBook);
+    dialog.close();
+  }
 
-  const title = getInputElementValue("#book-title");
-  const author = getInputElementValue("#book-author");
-  const pages = Number(getInputElementValue("#book-pages"));
-  const read = getCheckedElementBool("#book-read");
+  function dialogShowModal() {
+    closeModal.addEventListener("click", dialogCloseModal);
+    form.addEventListener("submit", processBook);
+    dialog.showModal();
+  }
 
-  const book = new Book(title, author, pages, read);
-  if (!checkRenderedBooks(book)) {
-    addBookToLibrary([book]);
+  function removeBookFromList(evt: Event) {
+    // prettier-ignore
+    if(!(evt.target instanceof HTMLElement && evt.target.parentElement instanceof HTMLElement) ) return;
+    let bookParentContainer = evt.target.parentElement;
+    let bookDeletionIndex = Number(
+      bookParentContainer.getAttribute("data-index"),
+    );
+    updateLists(bookDeletionIndex, bookParentContainer);
+  }
+
+  // prettier-ignore
+  function updateLists(bookDeletionIndex: number, bookParentContainer: HTMLElement) {
+    myLibrary.splice(bookDeletionIndex, 1);
+    main.removeChild(bookParentContainer);
     displayLibrary();
   }
-  dialog.close();
-}
 
-function getInputElementValue(selector: string) {
-  const element = document.querySelector(selector) as HTMLInputElement;
-  return element ? element.value : "";
-}
+  function processBook(evt: Event) {
+    evt.preventDefault();
 
-function getCheckedElementBool(selector: string) {
-  const element = document.querySelector(selector) as HTMLInputElement;
-  return element ? element.checked : false;
-}
-function addBookToLibrary(books: Book[]) {
-  myLibrary = [...myLibrary, ...books];
-}
-// prettier-ignore
-function createSelectDropdownWidget(readStatus: boolean) {
-  const statuses = ["To Read", "Read", "Currently Reading"];
-  const selectedIndex = readStatus ? 1 : 0;
+    const title = getInputElementValue("#book-title");
+    const author = getInputElementValue("#book-author");
+    const pages = Number(getInputElementValue("#book-pages"));
+    const read = getCheckedElementBool("#book-read");
 
-  const optionsHTML = statuses
-    .map((status, index) =>
-        `<option value="${status}" ${index === selectedIndex ? "selected" : ""}>${status}</option>`)
-    .join("");
+    const book = makeBook(title, author, pages, read);
+    if (!checkRenderedBooks(book)) {
+      addBookToLibrary(book);
+      displayLibrary();
+    }
+    dialog.close();
+  }
 
-  return `
+  function getInputElementValue(selector: string) {
+    const element = document.querySelector(selector) as HTMLInputElement;
+    return element ? element.value : "";
+  }
+
+  function getCheckedElementBool(selector: string) {
+    const element = document.querySelector(selector) as HTMLInputElement;
+    return element ? element.checked : false;
+  }
+  function addBookToLibrary(book: Book) {
+    myLibrary = [...myLibrary, book];
+  }
+  // prettier-ignore
+  function createSelectDropdownWidget(readStatus: boolean) {
+    const statuses = ["To Read", "Read", "Currently Reading"];
+    const selectedIndex = readStatus ? 1 : 0;
+
+    const optionsHTML = statuses
+        .map((status, index) =>
+            `<option value="${status}" ${index === selectedIndex ? "selected" : ""}>${status}</option>`)
+        .join("");
+
+    return `
     <div class="select">
         <select>
             ${optionsHTML}
@@ -79,40 +123,40 @@ function createSelectDropdownWidget(readStatus: boolean) {
         <span class="focus"></span>
     </div>
   `;
-}
-
-function updateDataIndexAttribute() {
-  let books = document.querySelectorAll(".book");
-  for (let [index, book] of books.entries()) {
-    book.setAttribute("data-index", String(index));
   }
-}
-// prettier-ignore
-function attachEventListenersToClass(className: string, eventType: any, eventHandler: any) {
-  const elements = document.querySelectorAll("." + className);
-  elements.forEach((element) => {
-    element.removeEventListener(eventType, eventHandler);
-    element.addEventListener(eventType, eventHandler);
-  });
-}
 
-function removeBookEventHandler(evt: Event) {
-  removeBookFromList(evt);
-}
-function displayLibrary() {
-  myLibrary.forEach((book, index) => {
-    if (!checkRenderedBooks(book)) {
-      const bookCardHTML = createBookCard(book, index);
-      main.insertAdjacentHTML("beforeend", bookCardHTML);
+  function updateDataIndexAttribute() {
+    let books = document.querySelectorAll(".book");
+    for (let [index, book] of books.entries()) {
+      book.setAttribute("data-index", String(index));
     }
-  });
-  updateDataIndexAttribute();
-  attachEventListenersToClass("delete-book", "click", removeBookEventHandler);
-}
+  }
+  // prettier-ignore
+  function attachEventListenersToClass(className: string, eventType: string, eventHandler: EventListenerOrEventListenerObject) {
+    const elements = document.querySelectorAll("." + className);
+    elements.forEach((element) => {
+      element.removeEventListener(eventType, eventHandler);
+      element.addEventListener(eventType, eventHandler);
+    });
+  }
 
-function createBookCard(book: Book, index: number) {
-  const readStatusDropdown = createSelectDropdownWidget(book.read);
-  return `
+  function removeBookEventHandler(evt: Event) {
+    removeBookFromList(evt);
+  }
+  function displayLibrary() {
+    myLibrary.forEach((book, index) => {
+      if (!checkRenderedBooks(book)) {
+        const bookCardHTML = createBookCard(book, index);
+        main.insertAdjacentHTML("beforeend", bookCardHTML);
+      }
+    });
+    updateDataIndexAttribute();
+    attachEventListenersToClass("delete-book", "click", removeBookEventHandler);
+  }
+
+  function createBookCard(book: Book, index: number) {
+    const readStatusDropdown = createSelectDropdownWidget(book.read);
+    return `
     <div class="book" data-index="${index}">
         <div class="book-info">
             <div class="title">${book.title}</div>
@@ -122,41 +166,20 @@ function createBookCard(book: Book, index: number) {
         </div>
         <button class="delete-book">x</button>
     `;
-}
-
-function checkRenderedBooks(book: Book) {
-  for (let bookContainer of main.children) {
-    let [bookTitle, bookAuthor] = [
-      bookContainer.children[0].children[0].textContent,
-      bookContainer.children[0].children[1].textContent,
-    ];
-    if (bookTitle === book.title && bookAuthor === book.author) return true;
   }
-  return false;
-}
 
-class Book {
-  readonly title: string;
-  readonly author: string;
-  readonly pages: number;
-  readonly read: boolean;
-  constructor(title: string, author: string, pages: number, read: boolean) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  function checkRenderedBooks(book: Book) {
+    for (let bookContainer of main.children) {
+      let bookTitle = bookContainer.querySelector(".title")?.textContent;
+      let bookAuthor = bookContainer.querySelector(".author")?.textContent;
+      if (bookTitle === book.title && bookAuthor === book.author) return true;
+    }
+    return false;
   }
-}
 
-/*
-    Initial library
- */
-
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-
-const theStranger = new Book("The Stranger", "Albert Camus", 125, true);
-// prettier-ignore
-const callMeByYourName = new Book("Call Me By Your Name", "Andre Aciman", 230, true);
-addBookToLibrary([theHobbit, theStranger, callMeByYourName]);
-
-displayLibrary();
+  return {
+    makeBook,
+    addBookToLibrary,
+    displayLibrary,
+  };
+})();
